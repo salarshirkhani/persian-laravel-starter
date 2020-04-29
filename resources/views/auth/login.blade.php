016e1d5cea0e4d65926f4ff67229142300fc5792
najafimh <m.hussein.najafi@gmail.com>
2020-04-29 23:48:04 +0430

    Finish authentication system

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,66 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    @if ($errors->any())
+        <div class="wrap-messages">
+            @foreach ($errors->all() as $error)
+            <div class="alert alert-danger">{{ $error }}</div>
+            @endforeach
         </div>
+    @endif
+    <div class="login100-pic js-tilt" data-tilt="">
+        <img src="{{ asset("assets/auth/images/auth.webp") }}" alt="IMG">
     </div>
-</div>
+
+    <form class="login100-form validate-form" action="{{ route('login') }}" method="POST">
+        @csrf
+        <span class="login100-form-title">
+            ورود به حساب
+					</span>
+
+        <div class="wrap-input100 validate-input" data-validate="ایمیل اجباری است!">
+            <input type="email" name="email" maxlength="100" class=" input100" placeholder="ایمیل" required=""
+                   id="id_email" value="{{ old('email') ?? '' }}">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+        </div>
+
+-
+        <div class="wrap-input100 validate-input" data-validate="رمزعبور اجباری است!">
+            <input type="password" name="password" class=" input100" placeholder="کلمه‌عبور" id="id_password">
+            <span class="focus-input100"></span>
+            <span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+        </div>
+
+        <div class="container-login100-form-btn">
+            <button class="login100-form-btn">
+                ورود
+            </button>
+        </div>
+
+        {{--        <div class="text-center p-t-12">--}}
+        {{--            <a class="txt2" href="#">--}}
+        {{--                رمزعبور خود--}}
+        {{--            </a>--}}
+        {{--            <span class="txt1">--}}
+        {{--							را فراموش کرده‌اید؟--}}
+        {{--						</span>--}}
+        {{--        </div>--}}
+
+        <div class="text-center p-t-136">
+            <a class="txt2" href="{{ route('register') }}">
+                {{ __('میخواهید ثبت‌نام کنید؟') }}
+                <i class="fa fa-long-arrow-left m-l-5" aria-hidden="true"></i>
+            </a>
+            <br>
+            <a class="txt2" href="{{ route('index') }}">
+                {{ __('بازگشت') }}
+                <i class="fa fa-long-arrow-left m-l-5" aria-hidden="true"></i>
+            </a>
+        </div>
+    </form>
 @endsection
