5ea2dea970c6832f889e33b18f8111aa602df08f
M. Hosseyn Najafi <hosseynjf@icloud.com>
2020-05-03 03:23:12 +0430

    Delete unused partial views

--- resources/views/dashboard/partials/multichoice/normal-unchecked.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="form-group col-6 col-md-2">
-    <div class="custom-control custom-checkbox">
-        <input type="radio" class="custom-control-input normal-answer" id="q{{ $number }}_o-1" name="{{ $number }}"
-               value="-1" @if($selected) checked="checked" @endif>
-        <label class="custom-control-label" for="q{{ $number }}_o-1">سوال نزده</label>
-    </div>
-</div>
