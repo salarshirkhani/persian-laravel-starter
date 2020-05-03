04bb8b2fe58df0fc52fbcb52b918815cbff65c01
M. Hosseyn Najafi <hosseynjf@icloud.com>
2020-05-04 02:45:29 +0430

    Add CompanyPolicy and tests

---
+++ app/Policies/CompanyPolicy.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Policies;
+
+use App\Company;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CompanyPolicy
+{
+    use HandlesAuthorization;
+
+    public function before(User $user, $ability)
+    {
+        if ($user->type == 'admin')
+            return true;
+        return null;
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Company  $company
+     * @return mixed
+     */
+    public function view(User $user, Company $company)
+    {
+        return $user->type == 'owner' && !empty($company->creator) && $company->creator->is($user);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->type == 'owner';
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Company  $company
+     * @return mixed
+     */
+    public function update(User $user, Company $company)
+    {
+        return $user->type == 'owner' && !empty($company->creator) && $company->creator->is($user);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Company  $company
+     * @return mixed
+     */
+    public function delete(User $user, Company $company)
+    {
+        return $user->type == 'owner' && !empty($company->creator) && $company->creator->is($user);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Company  $company
+     * @return mixed
+     */
+    public function restore(User $user, Company $company)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Company  $company
+     * @return mixed
+     */
+    public function forceDelete(User $user, Company $company)
+    {
+        return false;
+    }
+}
---
+++ tests/Unit/Policy/CompanyPolicyTest.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Tests\Unit\Policy;
+
+use App\Company;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CompanyPolicyTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_admins_full_access() {
+        $user = factory(User::class)->make(['type' => 'admin']);
+        $company = factory(Company::class)->state('product')->make();
+
+        $this->assertTrue($user->can('viewAny', Company::class));
+        $this->assertTrue($user->can('view', $company));
+        $this->assertTrue($user->can('create', Company::class));
+        $this->assertTrue($user->can('update', $company));
+        $this->assertTrue($user->can('delete', $company));
+        $this->assertTrue($user->can('restore', $company));
+        $this->assertTrue($user->can('forceDelete', $company));
+    }
+
+    ///////////
+
+    public function test_users_cannot_view_company_list() {
+        $user = factory(User::class)->make();
+        $this->assertFalse($user->can('viewAny', Company::class));
+    }
+
+    ///////////
+
+    public function test_users_cannot_view_company() {
+        $user = factory(User::class)->make();
+        $company = factory(Company::class)->state('product')->make();
+        $this->assertFalse($user->can('view', $company));
+    }
+
+    public function test_former_owners_cannot_view_company() {
+        $user = factory(User::class)->create(['type' => 'customer']);
+        $company = factory(Company::class)->state('product')->make(['creator_id' => $user->getKey()]);
+        $this->assertFalse($user->can('view', $company));
+    }
+
+    public function test_all_owners_cannot_view_company() {
+        $user = factory(User::class)->create(['type' => 'owner']);
+        $company = factory(Company::class)->state('product')->make();
+        $this->assertFalse($user->can('view', $company));
+    }
+
+    public function test_company_owner_can_view_company() {
+        $user = factory(User::class)->create(['type' => 'owner']);
+        $company = factory(Company::class)->state('product')->make(['creator_id' => $user->getKey()]);
+        $this->assertTrue($user->can('view', $company));
+    }
+
+    ///////////
+
+    public function test_customers_cannot_create_company() {
+        $user = factory(User::class)->make(['type' => 'customer']);
+        $this->assertFalse($user->can('create', Company::class));
+    }
+
+    public function test_owners_can_create_company() {
+        $user = factory(User::class)->make(['type' => 'owner']);
+        $this->assertTrue($user->can('create', Company::class));
+    }
+
+    ///////////
+
+    public function test_users_cannot_update_company() {
+        $user = factory(User::class)->make();
+        $company = factory(Company::class)->state('product')->make();
+        $this->assertFalse($user->can('update', $company));
+    }
+
+    public function test_former_owners_cannot_update_company() {
+        $user = factory(User::class)->create(['type' => 'customer']);
+        $company = factory(Company::class)->state('product')->make(['creator_id' => $user->getKey()]);
+        $this->assertFalse($user->can('update', $company));
+    }
+
+    public function test_all_owners_cannot_update_company() {
+        $user = factory(User::class)->create(['type' => 'owner']);
+        $company = factory(Company::class)->state('product')->make();
+        $this->assertFalse($user->can('update', $company));
+    }
+
+    public function test_company_owner_can_update_company() {
+        $user = factory(User::class)->create(['type' => 'owner']);
+        $company = factory(Company::class)->state('product')->make(['creator_id' => $user->getKey()]);
+        $this->assertTrue($user->can('update', $company));
+    }
+
+    ///////////
+
+    public function test_users_cannot_delete_company() {
+        $user = factory(User::class)->make();
+        $company = factory(Company::class)->state('product')->make();
+        $this->assertFalse($user->can('delete', $company));
+    }
+
+    public function test_former_owners_cannot_delete_company() {
+        $user = factory(User::class)->create(['type' => 'customer']);
+        $company = factory(Company::class)->state('product')->make(['creator_id' => $user->getKey()]);
+        $this->assertFalse($user->can('delete', $company));
+    }
+
+    public function test_all_owners_cannot_delete_company() {
+        $user = factory(User::class)->create(['type' => 'owner']);
+        $company = factory(Company::class)->state('product')->make();
+        $this->assertFalse($user->can('delete', $company));
+    }
+
+    public function test_company_owner_can_delete_company() {
+        $user = factory(User::class)->create(['type' => 'owner']);
+        $company = factory(Company::class)->state('product')->make(['creator_id' => $user->getKey()]);
+        $this->assertTrue($user->can('delete', $company));
+    }
+
+    ///////////
+
+    public function test_nobody_can_restore_company() {
+        $user = factory(User::class)->create();
+        $company = factory(Company::class)->state('product')->make();
+
+        $this->assertFalse($user->can('restore', $company));
+
+        $company->creator_id = $user->getKey();
+        $this->assertFalse($user->can('restore', $company));
+
+        $user->type = 'owner';
+        $company->creator_id = null;
+        $this->assertFalse($user->can('restore', $company));
+
+        $user->type = 'owner';
+        $company->creator_id = $user->getKey();
+        $this->assertFalse($user->can('restore', $company));
+
+        $user->type = 'customer';
+        $company->creator_id = null;
+        $this->assertFalse($user->can('restore', $company));
+
+        $user->type = 'customer';
+        $company->creator_id = $user->getKey();
+        $this->assertFalse($user->can('restore', $company));
+    }
+
+    ///////////
+
+    public function test_nobody_can_force_delete_company() {
+        $user = factory(User::class)->create();
+        $company = factory(Company::class)->state('product')->make();
+
+        $this->assertFalse($user->can('forceDelete', $company));
+
+        $company->creator_id = $user->getKey();
+        $this->assertFalse($user->can('forceDelete', $company));
+
+        $user->type = 'owner';
+        $company->creator_id = null;
+        $this->assertFalse($user->can('forceDelete', $company));
+
+        $user->type = 'owner';
+        $company->creator_id = $user->getKey();
+        $this->assertFalse($user->can('forceDelete', $company));
+
+        $user->type = 'customer';
+        $company->creator_id = null;
+        $this->assertFalse($user->can('forceDelete', $company));
+
+        $user->type = 'customer';
+        $company->creator_id = $user->getKey();
+        $this->assertFalse($user->can('forceDelete', $company));
+    }
+}
