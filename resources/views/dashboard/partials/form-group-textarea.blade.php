08c2a05ade99c0f0522dda3e75d3dfaf627966cc
M. Hosseyn Najafi <hosseynjf@icloud.com>
2020-05-07 21:56:56 +0430

    Remove old fom field types

--- resources/views/dashboard/partials/form-group-textarea.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="form-group">
-    <label for="input_{{ $name }}">{{ $label }}</label>
-    <textarea id="input_{{ $name }}" name="{{ $name }}" rows="{{ $rows ?? 5 }}"@if(($disabled ?? false))disabled @endif class="form-control @isset($class) {{ $class }} @endisset">{{ old($name) ?? ($value ?? '') }}</textarea>
-</div>
--- resources/views/dashboard/partials/form-group.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="form-group">
-    <label for="input_{{ $name }}">{{ $label }}</label>
-    <input type="{{ $type ?? 'text' }}" id="input_{{ $name }}" name="{{ $name }}" value="{{ old($name) ?? ($value ?? '') }}" @isset($min) min="{{ $min }}" @endisset @if(($disabled ?? false))disabled @endif class="form-control">
-</div>
