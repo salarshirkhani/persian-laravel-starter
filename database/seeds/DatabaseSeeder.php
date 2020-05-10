be2c28c8d3964b2d2a935952d2bfeff796cbcb85
M. Hosseyn Najafi <hosseynjf@icloud.com>
2020-05-10 19:45:52 +0430

    Add database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Company;
+use App\Product;
+use App\Service;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $companyTypes = ['product', 'service'];
+        factory(User::class, 6)->create(['type' => 'owner'])->each(function (User $user) use ($companyTypes) {
+            /** @var Company $company */
+            $user->company()->save(
+                $company = factory(Company::class)->states(['withPhoto', $companyTypes[array_rand($companyTypes)]])->make()
+            );
+            if ($company->type == "product")
+                $company->products()->saveMany(factory(Product::class, 20)->state('withPhoto')->make());
+            else
+                $company->services()->saveMany(factory(Service::class, 20)->state('withPhoto')->make());
+        });
+        factory(User::class, 5)->create(['type' => 'customer']);
     }
 }
