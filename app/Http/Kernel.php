c6a3f61dce05788573ed7ac84f247a3eed8e8909
M. Hosseyn Najafi <hosseynjf@icloud.com>
2020-06-14 23:25:19 +0430

    Add superuser middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'user_type' => \App\Http\Middleware\CheckUserType::class,
+        'superuser' => \App\Http\Middleware\CheckForSuperuser::class,
     ];
 }
